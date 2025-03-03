7518cf2bbd0bcb361d5dab9460d1f538ea4933cc
Sadek Rayhan <sadek.rayhan@selise.ch>
2025-03-03 15:56:02 +0200

    feat: updated deploy related files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC = () => {
       const formData = new FormData();
       formData.append('file', file);
 
-      const response = await axios.post('http://localhost:5000/predict', formData, {
+      const response = await axios.post('https://rockpaprescissor-ml-flask-react.onrender.com/predict', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
 
